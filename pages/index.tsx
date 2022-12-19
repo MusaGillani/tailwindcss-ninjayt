8084bf35ceeabf784850465e1f8caa196c3ec41e
Musa Gillani <musagillaniwork@gmail.com>
2022-12-19 16:48:55 +0500

    Video 14

--- pages/index.tsx
+++ pages/index.tsx
@@ -71,7 +71,22 @@ function Home() {
                   Recipe by mario
                 </span>
               </div>
-              <div className="badge">
+              <div className="badge flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+
                 <span>25 mins</span>
               </div>
             </div>
@@ -87,7 +102,21 @@ function Home() {
                   Recipe by mario
                 </span>
               </div>
-              <div className="badge">
+              <div className="badge flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
                 <span>25 mins</span>
               </div>
             </div>
@@ -103,7 +132,21 @@ function Home() {
                   Recipe by mario
                 </span>
               </div>
-              <div className="badge">
+              <div className="badge flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
                 <span>25 mins</span>
               </div>
             </div>
